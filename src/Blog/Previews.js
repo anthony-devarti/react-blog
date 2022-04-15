0c63c3dfe6b634afc0b6eaa55fcfcd77517aa8fc
Anthony DeVarti <anthonydevarti@DESKTOP-A1AM7U8.localdomain>
2022-04-15 09:48:45 -0400

    got the horizontal scroll working on the blog teasters

--- src/Blog/Previews.js
+++ src/Blog/Previews.js
@@ -13,14 +13,14 @@ export default function Previews(){
               return (
                 <>
                 <div className="col col">
-                    <div href="blogposts/onboarding/week1/week1.html" className="card card-cover h-100 overflow-hidden text-white bg-dark rounded-5 shadow-lg card" style={{maxWidth:'80vw', alignitems:'center'}} key={post.id}>
+                    <div className="card card-cover h-100 overflow-hidden text-white bg-dark rounded-5 shadow-lg card scrollable" style={{maxWidth:'80vw', maxHeight:"400px", alignitems:'center'}} key={post.id}>
                         <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
                             <h2 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">{post.title}</h2>
-                            <ul className="d-flex list-unstyled mt-auto">
+                            <ul className="d-flex mt-auto">
                                 <li className="d-flex align-items-center me-3">
                                     <svg className="bi me-2" width="1em" height="1em"></svg>
                                     <small>{post.preview}</small></li>
-                                    <li className="d-flex align-items-center"><a href="something" className="btn btn-lg btn-secondary fw-bold mt-5">Read</a>
+                                    <li className="d-flex align-items-start"><a href='something' className="btn btn-lg btn-secondary fw-bold mt-5">Read</a>
                                     <small className="small-date">{post.date}</small>              
                                 </li>
                             </ul>
