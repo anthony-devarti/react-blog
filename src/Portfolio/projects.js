d1b42b7f80b4c40bcf68e1cdec6c134f2f5ac3f8
Anthony DeVarti <anthony.devarti@student.awesomeinc.org>
2022-04-16 03:32:07 +0000

    project previews are rendering dynamically now.  I still need to add the pictures and the tech stack icons

---
+++ src/Portfolio/projects.js
@@ -0,0 +1,134 @@
+export const projects = [
+    
+    {
+        "id": "0",
+        "title": "The Example Project",
+        "teaser": "This is a teaser for the example project.",
+        "link": "#",
+        "photo": "./images/screenshot",
+        "stack": "[html, js, react]",
+        "date": "09/24/21",
+        "week": "0"
+    },
+    {
+        "id": "1",
+        "title": "The First Blog Post",
+        "teaser": "Learning Github and gh pages.",
+        "link": "https://github.com/anthony-devarti/anthony-devarti.github.io",
+        "photo": "./images/screenshot",
+        "stack": "[markdown]",
+        "date": "01/26/22",
+        "week": "1"
+    },
+    {
+        "id": "2",
+        "title": "Structure Page of Content",
+        "teaser": "HTML and CSS basics via birdwatching.",
+        "link": "#",
+        "photo": "./images/screenshot",
+        "stack": "[html, css]",
+        "date": "02/04/22",
+        "week": "2"
+    },
+    {
+        "id": "3",
+        "title": "Silly Story Generator",
+        "teaser": "Using JS to make a silly story",
+        "link": "https://github.com/anthony-devarti/SillyStoryGenerator",
+        "photo": "./images/screenshot",
+        "stack": "[javascript,]",
+        "date": "02/11/22",
+        "week": "3"
+    },
+    {
+        "id": "4",
+        "title": "Bouncing Balls",
+        "teaser": "A simple game in Javascript.",
+        "link": "#",
+        "photo": "./images/screenshot",
+        "stack": "[javascript, html, css]",
+        "date": "02/18/22",
+        "week": "4"
+    },
+    {
+        "id": "5",
+        "title": "Mind Reader",
+        "teaser": "A math trick displayed visually.",
+        "link": "#",
+        "photo": "./images/screenshot",
+        "stack": "[html, javascript, bootstrap, css]",
+        "date": "02/25/22",
+        "week": "5"
+    },
+    {
+        "id": "6",
+        "title": "Weather App",
+        "teaser": "Dynamically displaying the weather using a free API.",
+        "link": "#",
+        "photo": "./images/screenshot",
+        "stack": "[html, js, react]",
+        "date": "03/02/22",
+        "week": "6"
+    },
+    {
+        "id": "7",
+        "title": "Tic-Tac-Toe",
+        "teaser": "First looks at MVC design pattern.",
+        "link": "#",
+        "photo": "./images/screenshot",
+        "stack": "[html, javascrip, css]",
+        "date": "03/13/22",
+        "week": "7"
+    },
+    {
+        "id": "8",
+        "title": "React Restaurant",
+        "teaser": "First look at react.",
+        "link": "#",
+        "photo": "./images/screenshot",
+        "stack": "[html, javascript, react]",
+        "date": "03/26/22",
+        "week": "8"
+    },
+    {
+        "id": "9",
+        "title": "To-Do List",
+        "teaser": "Making a todo list with local storage.",
+        "link": "#",
+        "photo": "./images/screenshot",
+        "stack": "[html, javascript, react]",
+        "date": "03/30/22",
+        "week": "9"
+    },
+    {
+        "id": "10",
+        "title": "Evercraft",
+        "teaser": "Designing a classic RPG in Python.",
+        "link": "#",
+        "photo": "./images/screenshot",
+        "stack": "[python,]",
+        "date": "04/01/22",
+        "week": "10"
+    },
+    {
+        "id": "11",
+        "title": "SQL Heroes",
+        "teaser": "Building up an interlinked database.",
+        "link": "#",
+        "photo": "./images/screenshot",
+        "stack": "[postgres]",
+        "date": "04/07/22",
+        "week": "11"
+    },
+    {
+        "id": "12",
+        "title": "Django To-do",
+        "teaser": "Building a backend for our todo list.",
+        "link": "#",
+        "photo": "./images/screenshot",
+        "stack": "[postgres, django, rest]",
+        "date": "04/15/22",
+        "week": "12"
+    },
+  
+]
